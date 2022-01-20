2570368104a6edec5a6fff2e9bc533f744bfa795
Code Sandbox <hello@codesandbox.io>
2022-01-20 15:18:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e53047a3cc283742eab649964fc71c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1318f295011fdb1085a8baf2b251e3",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/index.html"
+  },
+  {
+    "revision": "015779776a1021c9698f",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/css/main.f59dff40.chunk.css"
+  },
+  {
+    "revision": "e2caa56471c107a3e3f0",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/js/2.995a9cff.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/js/2.995a9cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015779776a1021c9698f",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/js/main.75d97339.chunk.js"
+  },
+  {
+    "revision": "3bbeb4000b8d2a7f556a",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/js/runtime-main.6669dc2c.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "https://rickallmighty8195.github.io/csb-lq4zm/static/media/logo.be622899.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://rickallmighty8195.github.io/csb-lq4zm/precache-manifest.e53047a3cc283742eab649964fc71c42.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rickallmighty8195.github.io/csb-lq4zm/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
